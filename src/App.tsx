0f08ee1fdb84dc003fec64470210238a5890532b
Raveena Ingale <ingale.r@northeastern.edu>
2024-03-08 19:05:05 -0500

    fix:routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
