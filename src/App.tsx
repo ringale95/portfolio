c71086193200d689996b3f858f6166087fc23b4d
Raveena Ingale <ingale.r@northeastern.edu>
2024-03-08 19:55:23 -0500

    fix: body bg color change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 function App() {
